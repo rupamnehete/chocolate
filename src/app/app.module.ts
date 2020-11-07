d188f300aa7e712d29da962005474406093b08a1
rupamnehete <rupamnehete@gmail.com>
2020-11-07 21:53:44 +0530

    chocolate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SlideImagesComponent } from './slide-images/slide-images.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ProductComponent } from './product/product.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SlideImagesComponent,
+    ContactUsComponent,
+    ProductComponent,
+    AboutUsComponent,
+    SignInComponent,
+    RegistrationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/slide-images/slide-images.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SlideImagesComponent } from './slide-images.component';
+
+describe('SlideImagesComponent', () => {
+  let component: SlideImagesComponent;
+  let fixture: ComponentFixture<SlideImagesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SlideImagesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SlideImagesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
